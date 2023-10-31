c48407218b0f40dd9efc0f1d1dccff69183edf48
José Rosendo <jose.l.rosendo.s@gmail.com>
2023-10-31 16:56:39 -0300

    Add TODO comment about matching engine

--- orderbook.js
+++ orderbook.js
@@ -13,6 +13,11 @@ class Orderbook {
     } else {
       this.sellOrders.push(order);
     }
+
+    // TODO: after adding the order, we should run the simple order matching engine logic
+    // if any remainder is created, we should return a signal that indicates to the caller of the
+    // addOrder method that the order was not fully filled and hence new orders were created
+    // so the called should broadcast the new orders to the network
   }
 }
 
